08c9f02fb3bb013d8f9ae474d968b6c03a1978aa
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-11-30 21:44:24 +0100

    restyled grammar and outline components

--- pages/grammar.tsx
+++ pages/grammar.tsx
@@ -1,63 +1,131 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { Navbar } from "../components/Navbar";
+import { Options } from "../components/options";
 import { homeboxes, openai } from "../utils";
 
 export default function Grammar() {
   const { pathname } = useRouter();
   const pageProps = homeboxes.find((homebox) => homebox.link === pathname)!;
+  const [option, setOption] = useState("text-ada-001");
+
+  const changeOption = (inputOption: string) => {
+    setOption(inputOption);
+  };
 
   const [prompt, setPrompt] = useState<string | undefined>("");
   const [result, setResult] = useState<string | undefined>("");
-  async function summarizeData() {
-    const { data } = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Correct this to standard English:" + "\n" + `${prompt}`,
-      max_tokens: 1000,
-      temperature: 0,
-    });
-    console.log(data);
-    setResult(data.choices[0].text);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  async function simplifyTextData() {
+    setLoading(true);
+    await openai
+      .createCompletion({
+        model: option,
+        prompt: "Correct this to standard English:" + "\n" + `${prompt}`,
+        max_tokens: 1000,
+        temperature: 0,
+      })
+      .then((response) => {
+        const { data } = response;
+        setResult(data.choices[0].text);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError("An error occured. Please wait for a minute and then retry.");
+      });
   }
 
   return (
-    <main className="min-h-screen bg-slate-900 font-alpino">
-      <div className="max-w-[1200px] mx-auto pt-16">
-        <section className="flex justify-between items-center w-[95%] md:w-full mx-auto">
-          <div
-            style={{ backgroundColor: pageProps.colorOne }}
-            className="lg:h-[200px] lg:w-[200px] h-[100px] w-[100px]
+    <main className="min-h-screen bg-slate-900 font-alpino text-white">
+      <div className="max-w-[1200px] py-16 flex justify-between mx-auto flex-wrap">
+        <div className="lg:w-5/12 w-11/12 mx-auto">
+          <Navbar color={pageProps.colorOne} />
+          <div className="flex">
+            <div
+              style={{ backgroundColor: pageProps.colorOne }}
+              className="lg:h-[110px] lg:w-[110px] h-[100px] w-[100px]
        rounded-sm text-white text-6xl flex items-center shadow-md
+       mr-6
         justify-center"
-          >
-            <h1>{pageProps.no}</h1>
+            >
+              <h1>{pageProps.no}</h1>
+            </div>
+            <div>
+              <div className="text-white max-w-[280px]">
+                <h1 className="lg:text-5xl md:text-5xl text-4xl font-bold">
+                  {pageProps.description}
+                </h1>
+                <p className="md:text-xl text-md mt-2">{pageProps.title}</p>
+              </div>
+            </div>
           </div>
-          <div className="text-white ml-4 md:ml-0">
-            <h1 className="lg:text-6xl md:text-5xl text-4xl font-bold">
-              {pageProps.description}
-            </h1>
-            <p className="md:text-xl text-md">{pageProps.title}</p>
+          <div>
+            <h1 className="font-medium text-lg mt-5">Example Prompt</h1>
+            <p className="mb-4 border p-2 mt-2">I is not have many sense.</p>
           </div>
-        </section>
-        <section className="flex mt-24  justify-between flex-wrap">
-          <main className="lg:w-5/12 w-[95%]  mx-auto md:mx-0">
-            <textarea
-              placeholder="Enter the text/paragraph you want to correct here"
-              className="min-h-[150px] text-xl p-2 bg-slate-800 w-full text-white resize-none"
-              onChange={(e) => setPrompt(e.target.value)}
-            ></textarea>
+          <div>
+            <h1 className="font-medium text-lg">Example Response</h1>
+            <p className="mb-4 border p-2 mt-2">I do not have much sense.</p>
+          </div>
+          <Options
+            color={pageProps.colorOne}
+            option={option}
+            setOption={changeOption}
+          />
+          <textarea
+            placeholder="Enter the text you want to generate a simplified version for here."
+            className="min-h-[150px] text-xl p-2 bg-slate-800 w-full text-white resize-none"
+            onChange={(e) => setPrompt(e.target.value)}
+          ></textarea>
+          {!loading ? (
             <button
-              onClick={summarizeData}
+              onClick={simplifyTextData}
               style={{ backgroundColor: pageProps.colorOne }}
               className=" text-white px-2 py-1 text-xl mt-4 rounded-sm"
             >
-              Generate Essay
+              Correct Text
             </button>
-          </main>
-          <aside className="text-white md:w-6/12 w-[95%] mx-auto mt-8 md:mt-0">
-            <h1 className="text-3xl font-bold">Response</h1>
-            {result !== "" ? <p className="text-xl mt-4">{result}</p> : null}
-          </aside>
-        </section>
+          ) : (
+            <button
+              onClick={simplifyTextData}
+              disabled
+              style={{ backgroundColor: pageProps.colorOne }}
+              className=" text-white px-2 py-1 text-xl mt-4 rounded-sm opacity-90"
+            >
+              Loading...
+            </button>
+          )}
+        </div>
+        <div className="lg:w-5/12 w-11/12 mx-auto">
+          <h1 className="text-3xl font-bold mt-16">Response</h1>
+          {result !== "" ? (
+            <>
+              <button
+                onClick={() => window.navigator.clipboard.writeText(result!)}
+                className="mb-3 self-end mt-8 p-2 font-medium"
+                style={{ backgroundColor: pageProps.colorOne }}
+              >
+                Copy Text
+              </button>
+              <div
+                className="min-h-[150px] text-xl p-2 bg-slate-800 w-full text-white resize-none "
+                dangerouslySetInnerHTML={{
+                  __html: `<p>${result}</p>`,
+                }}
+              ></div>
+            </>
+          ) : null}
+          {error !== "" ? (
+            <div
+              className="min-h-[150px] text-xl p-2 bg-slate-800 w-full text-white resize-none mt-8"
+              dangerouslySetInnerHTML={{
+                __html: `<p>${error}</p>`,
+              }}
+            ></div>
+          ) : null}
+        </div>
       </div>
     </main>
   );
